21e75203dbe188018ca42bc9055003bf3279a005
unknown <henderljunior@gmail.com>
2026-04-28 00:32:54 -0400

    First Commit

---
+++ src/data/albumData.js
@@ -0,0 +1,145 @@
+export const albumMeta = {
+  totalStickers: 980,
+  totalCountries: 48,
+  stickersPerCountry: 20,
+  totalPages: 112,
+}
+
+export const specialStickers = [
+  '00',
+  'FWC 1',
+  'FWC 2',
+  'FWC 3',
+  'FWC 4',
+  'FWC 5',
+  'FWC 6',
+  'FWC 7',
+  'FWC 8',
+  'CUP 1',
+  'CUP 2',
+  'BALL 1',
+  'MASCOT 1',
+  'SLOGAN 1',
+  'FWC 9',
+  'FWC 10',
+  'FWC 11',
+  'FWC 12',
+  'FWC 13',
+  'FWC 14',
+  'FWC 15',
+  'FWC 16',
+  'FWC 17',
+  'FWC 18',
+  'FWC 19',
+]
+
+export const groups = [
+  {
+    id: 'A',
+    countries: [
+      { name: 'Mexico', code: 'MEX', flag: 'MX' },
+      { name: 'Chequia', code: 'CZE', flag: 'CZ' },
+      { name: 'Republica de Corea', code: 'KOR', flag: 'KR' },
+      { name: 'Sudafrica', code: 'RSA', flag: 'ZA' },
+    ],
+  },
+  {
+    id: 'B',
+    countries: [
+      { name: 'Canada', code: 'CAN', flag: 'CA' },
+      { name: 'Bosnia y Herzegovina', code: 'BIH', flag: 'BA' },
+      { name: 'Catar', code: 'QAT', flag: 'QA' },
+      { name: 'Suiza', code: 'SUI', flag: 'CH' },
+    ],
+  },
+  {
+    id: 'C',
+    countries: [
+      { name: 'Brasil', code: 'BRA', flag: 'BR' },
+      { name: 'Escocia', code: 'SCO', flag: 'GB' },
+      { name: 'Haiti', code: 'HAI', flag: 'HT' },
+      { name: 'Marruecos', code: 'MAR', flag: 'MA' },
+    ],
+  },
+  {
+    id: 'D',
+    countries: [
+      { name: 'EE. UU.', code: 'USA', flag: 'US' },
+      { name: 'Australia', code: 'AUS', flag: 'AU' },
+      { name: 'Paraguay', code: 'PAR', flag: 'PY' },
+      { name: 'Turquia', code: 'TUR', flag: 'TR' },
+    ],
+  },
+  {
+    id: 'E',
+    countries: [
+      { name: 'Alemania', code: 'GER', flag: 'DE' },
+      { name: 'Costa de Marfil', code: 'CIV', flag: 'CI' },
+      { name: 'Curazao', code: 'CUW', flag: 'CW' },
+      { name: 'Ecuador', code: 'ECU', flag: 'EC' },
+    ],
+  },
+  {
+    id: 'F',
+    countries: [
+      { name: 'Japon', code: 'JPN', flag: 'JP' },
+      { name: 'Paises Bajos', code: 'NED', flag: 'NL' },
+      { name: 'Suecia', code: 'SWE', flag: 'SE' },
+      { name: 'Tunez', code: 'TUN', flag: 'TN' },
+    ],
+  },
+  {
+    id: 'G',
+    countries: [
+      { name: 'Belgica', code: 'BEL', flag: 'BE' },
+      { name: 'Egipto', code: 'EGY', flag: 'EG' },
+      { name: 'Nueva Zelanda', code: 'NZL', flag: 'NZ' },
+      { name: 'RI de Iran', code: 'IRN', flag: 'IR' },
+    ],
+  },
+  {
+    id: 'H',
+    countries: [
+      { name: 'Arabia Saudi', code: 'KSA', flag: 'SA' },
+      { name: 'Espana', code: 'ESP', flag: 'ES' },
+      { name: 'Islas de Cabo Verde', code: 'CPV', flag: 'CV' },
+      { name: 'Uruguay', code: 'URU', flag: 'UY' },
+    ],
+  },
+  {
+    id: 'I',
+    countries: [
+      { name: 'Francia', code: 'FRA', flag: 'FR' },
+      { name: 'Irak', code: 'IRQ', flag: 'IQ' },
+      { name: 'Noruega', code: 'NOR', flag: 'NO' },
+      { name: 'Senegal', code: 'SEN', flag: 'SN' },
+    ],
+  },
+  {
+    id: 'J',
+    countries: [
+      { name: 'Argelia', code: 'ALG', flag: 'DZ' },
+      { name: 'Argentina', code: 'ARG', flag: 'AR' },
+      { name: 'Austria', code: 'AUT', flag: 'AT' },
+      { name: 'Jordania', code: 'JOR', flag: 'JO' },
+    ],
+  },
+  {
+    id: 'K',
+    countries: [
+      { name: 'Colombia', code: 'COL', flag: 'CO' },
+      { name: 'Portugal', code: 'POR', flag: 'PT' },
+      { name: 'RD Congo', code: 'COD', flag: 'CD' },
+      { name: 'Uzbekistan', code: 'UZB', flag: 'UZ' },
+    ],
+  },
+  {
+    id: 'L',
+    countries: [
+      { name: 'Croacia', code: 'CRO', flag: 'HR' },
+      { name: 'Ghana', code: 'GHA', flag: 'GH' },
+      { name: 'Inglaterra', code: 'ENG', flag: 'GB' },
+      { name: 'Panama', code: 'PAN', flag: 'PA' },
+    ],
+  },
+]
